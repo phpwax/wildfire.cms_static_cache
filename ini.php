7b098cd6f4390ce71e78e10427d584c459189daf
Charles Marshall <charles@oneblackbear.com>
2013-07-02 14:58:31 +0100

    play with assets

--- ini.php
+++ ini.php
@@ -1,8 +1,7 @@
 <?
 
-AutoLoader::register_assets("stylesheets/wildfire.cms_static_cache",__DIR__."/resources/public/stylesheets/wildfire.cms_static_cache/", "/*.css");
-AutoLoader::register_assets("javascripts/wildfire.cms_static_cache",__DIR__."/resources/public/javascripts/wildfire.cms_static_cache/", "/*.js");
-AutoLoader::register_assets("images/wildfire.cms_static_cache",__DIR__."/resources/public/images/wildfire.cms_static_cache/");
+AutoLoader::register_assets("stylesheets/wildfire.cms_static_cache",__DIR__."/resources/public/stylesheets/wildfire.cms_static_cache", "/*.css");
+#AutoLoader::register_assets("javascripts/wildfire.cms_static_cache",__DIR__."/resources/public/javascripts/wildfire.cms_static_cache", "/*.js");
 
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
