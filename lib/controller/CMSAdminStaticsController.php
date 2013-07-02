09e31a3e25c5ac9cd89ba1bc5464928261edde2e
Charles Marshall <charles@oneblackbear.com>
2013-07-02 15:33:39 +0100

    change what the filter searches on

--- lib/controller/CMSAdminStaticsController.php
+++ lib/controller/CMSAdminStaticsController.php
@@ -8,6 +8,9 @@ class CMSAdminStaticsController extends AdminComponent {
   public $singular = "Cache";
   public $per_page = 20;
   public $tree_layout = false;
+  public $filter_fields=array(
+                          'text' => array('columns'=>array('origin_url', 'title'), 'partial'=>'_filters_text', 'fuzzy'=>true)
+                        );
   public $operation_actions = array(
                                                   'regen'=>array('action'=>'regen', 'name'=>'Regenerate'),
                                                   'remove'=>array('action'=>'remove', 'name'=>'Remove')
