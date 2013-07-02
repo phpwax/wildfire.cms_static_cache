8e04185117a4c0f7b2364edfc86bb1d4b0144fda
Charles Marshall <charles@oneblackbear.com>
2013-07-02 11:29:09 +0100

    fix the class names being called to match new version

--- setup.php
+++ setup.php
@@ -1,16 +1,18 @@
 <?
 
 if(defined("CONTENT_MODEL")){
+
   WaxEvent::add("cms.save.success.finished", function(){
     $controller = WaxEvent::data();
     $obj = $controller->model;
     $class = get_class($obj);
+
     //if the class matches & readonly is enabled, then make the pages
-    if($class == CONTENT_MODEL && ReadOnly::is_enabled()){
+    if($class == CONTENT_MODEL && StaticCache::is_enabled($obj)){
       $source_url = "http://".$_SERVER['HTTP_HOST'].$obj->permalink."?readonly=1";
       $content = file_get_contents($source_url);
       $map_model = new WildfireUrlMap;
-      foreach($map_model->filter("destination_id", $obj->primval)->filter("destination_model", $class)->all() as $url) ReadOnly::create_cache($url->origin_url, $content);
+      foreach($map_model->filter("destination_id", $obj->primval)->filter("destination_model", $class)->all() as $url) StaticCache::create($url->origin_url, $content);
     }
   });
 }
