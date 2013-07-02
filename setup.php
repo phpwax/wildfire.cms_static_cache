248950c21000819f5bf38219404902594ba57869
Charles Marshall <charles@oneblackbear.com>
2013-07-02 11:54:42 +0100

    updated for function name changes

--- setup.php
+++ setup.php
@@ -21,11 +21,11 @@ if(defined("CONTENT_MODEL")){
     $class = get_class($obj);
 
     //if the class matches & readonly is enabled, then make the pages
-    if($class == CONTENT_MODEL && StaticCache::is_enabled($obj)){
+    if($class == CONTENT_MODEL && StaticCache::cacheable($obj)){
       $source_url = "http://".$_SERVER['HTTP_HOST'].$obj->permalink."?readonly=1";
       $content = file_get_contents($source_url);
       $map_model = new WildfireUrlMap;
-      foreach($map_model->filter("destination_id", $obj->primval)->filter("destination_model", $class)->all() as $url) StaticCache::create($url->origin_url, $content);
+      foreach($map_model->filter("destination_id", $obj->primval)->filter("destination_model", $class)->all() as $url) StaticCache::write($url, $content);
     }
   });
 }
