9f1c84509fccf441834ec65a1cb144b3508d8aaa
Charles Marshall <charles@oneblackbear.com>
2013-07-02 16:28:47 +0100

    working selection page based on the parent tree partial

--- lib/controller/CMSAdminStaticsController.php
+++ lib/controller/CMSAdminStaticsController.php
@@ -32,15 +32,14 @@ class CMSAdminStaticsController extends AdminContentController {
       $obj = WaxEvent::data();
       $obj->scaffold_columns = array('title'=>true, 'origin_url'=>true, 'date_cached'=>true);
     });
-    WaxEvent::clear("cms.model.init");
     WaxEvent::add("cms.model.init", function(){
       $obj = WaxEvent::data();
       $obj->model = new $obj->model_class($obj->model_scope);
       //add filter for active cache only
       $obj->model->filter("status",1)->filter("LENGTH(static_cache_file) > 0");
-
     });
   }
+
 
 
   public function _parent(){
@@ -49,11 +48,19 @@ class CMSAdminStaticsController extends AdminContentController {
   }
 
   public function selection(){
-    $this->model_class = CONTENT_MODEL;
+    $class = $this->model_class = CONTENT_MODEL;
     WaxEvent::run("cms.form.setup", $this);
 
-    $this->controller = "admin/content";
-
+    $posted = Request::param($this->model->table);
+    $page_id = $posted[$this->model->parent_join_field];
+    if($posted && $page_id && ($obj = new $class($page_id)) && StaticCache::cacheable($obj) ){
+      $source_url = "http://".$_SERVER['HTTP_HOST'].$obj->permalink."?readonly=1";
+      $content = file_get_contents($source_url);
+      $map_class = URL_MAP_MODEL;
+      $map_model = new $map_class;
+      foreach($map_model->filter("destination_id", $obj->primval)->filter("destination_model", $class)->all() as $url) StaticCache::write($url, $content);
+      $this->session->add_message("<a href='$source_url' target='_blank'>Cache for $obj->title</a> has been created.");
+    }elseif($posted && $page_id) $this->session->add_error("This page cannot be cached.");
   }
 
   public function regen(){
