49d10a0bc662f4efee8f599b6c6b5595cd051103
Charles Marshall <charles@oneblackbear.com>
2013-07-02 14:38:55 +0100

    assets

--- ini.php
+++ ini.php
@@ -1,3 +1,8 @@
 <?
+
+AutoLoader::register_assets("stylesheets/wildfire.cms_static_cache",__DIR__."/resources/public/stylesheets/wildfire.cms_static_cache/", "/*.css");
+AutoLoader::register_assets("javascripts/wildfire.cms_static_cache",__DIR__."/resources/public/javascripts/wildfire.cms_static_cache/", "/*.js");
+AutoLoader::register_assets("images/wildfire.cms_static_cache",__DIR__."/resources/public/images/wildfire.cms_static_cache/");
+
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
